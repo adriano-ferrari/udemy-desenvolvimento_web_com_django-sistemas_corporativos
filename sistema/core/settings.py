1dd1a945d3d0ed9f75c6b0ffcdcd3fdca4844ca5
adriano-ferrari <irarrefonairda@gmail.com>
2024-07-28 19:20:42 -0300

    Secao 4 | 24.10- Mensagens de Alerta

--- sistema/core/settings.py
+++ sistema/core/settings.py
@@ -14,6 +14,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -105,6 +106,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Apps
+                'base.context_processors.context_social',
             ],
         },
     },
@@ -207,3 +210,13 @@ EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+
+# Messages
+MESSAGE_TAGS = {
+	constants.ERROR: 'alert-danger',
+	constants.WARNING: 'alert-warning',
+	constants.DEBUG: 'alert-danger',
+	constants.SUCCESS: 'alert-success',
+	constants.INFO: 'alert-info',
+}
