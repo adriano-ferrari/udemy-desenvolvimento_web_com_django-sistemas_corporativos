b1c5db79f3b71841bc7e1c72f9d1a9ed12be4d46
adriano-ferrari <irarrefonairda@gmail.com>
2024-07-28 20:05:13 -0300

    Secao 5 | 27.2- Rota Session Timeout

--- sistema/core/urls.py
+++ sistema/core/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('contas/', include('contas.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
