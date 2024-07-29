8dbef0b8c7dc94931ebec677649bcd7b2f06c412
adriano-ferrari <irarrefonairda@gmail.com>
2024-07-28 23:39:31 -0300

    Secao 5 | 29.4- Rota de Cadastro

--- sistema/apps/contas/views.py
+++ sistema/apps/contas/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import Group, User
+
+from .forms import CustomUserCreationForm
 
 
 def timeout_view(request):
@@ -21,3 +24,26 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect('home')
     return render(request, 'login.html')
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            usuario.is_valid = False
+            usuario.save()
+
+            group = Group.objects.get(name='usuario')
+            usuario.groups.add(group)
+
+            messages.success(request, 'Registrado. Agora faça o login para começar!')
+            return redirect('login')
+        else:
+            # Tratar quando usuario já existe, senhas... etc...
+            messages.error(request, 'A senha deve ter pelo menos 1 caractere maiúsculo, \
+                1 caractere especial e no minimo 8 caracteres.')
+    form = CustomUserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
