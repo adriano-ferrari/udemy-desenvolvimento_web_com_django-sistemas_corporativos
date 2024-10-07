a1abd48b4cad5002aafc3a5e5a9f1c19b9987324
Adriano Ferrari <irarrefonairda@gmail.com>
2024-10-07 20:39:37 -0300

    Secao 4: Projeto Parte 1 - Configurações Iniciais | 24.10 - Mensagens de Alerta

--- site_sistema/apps/pages/views.py
+++ site_sistema/apps/pages/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    context = {
+        'mensagem': messages.success(request, 'Esta é uma mensagem de sucesso!')
+    }
     return render(request, 'pages/index.html')
