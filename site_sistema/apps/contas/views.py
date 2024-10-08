4838be0458311a8358ef54f016d1b1a6607a8563
adriano-ferrari <adriano-ferrari@outlook.com>
2024-10-08 08:16:43 -0300

    Seção 5: Projeto Parte 2 - Autenticação e Permissões de Acesso | Aula4_rota_de_cadastro

--- site_sistema/apps/contas/views.py
+++ site_sistema/apps/contas/views.py
@@ -1,13 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import Group, User
 
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+# Rota Timeout (desconecta por inatividade)
 def timeout_view(request):
     return render(request, 'contas/timeout.html')
 
-
+# Login
 def login_view(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -22,3 +24,26 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect('home')
     return render(request, 'contas/login.html')
+
+# Registrar
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            usuario.is_valid = False
+            usuario.save()
+
+            group = Group.objects.get(name='usuario')
+            usuario.groups.add(group)
+
+            messages.success(request, 'Registrado. Agora faça o login para começar!')
+            return redirect('login')
+        else:
+            # Tratar quando usuario já existe, senhas... etc...
+            messages.error(request, 'A senha deve ter pelo menos 1 caractere maiúsculo, \
+                1 caractere especial e no minimo 8 caracteres.')
+    form = CustomUserCreationForm()
+    return render(request, "contas/register.html", {"form": form})
