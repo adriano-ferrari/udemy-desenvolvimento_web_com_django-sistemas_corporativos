6fdd7ae243dc16839b1f9f4540bdfdbe78f8c9d2
adriano-ferrari <adriano-ferrari@outlook.com>
2024-10-13 15:41:58 -0300

    Seção 5: Projeto Parte 2 - Autenticação e Permissões de Acesso | Aula18_criar_perfil_de_usuario

--- site_sistema/apps/perfil/views.py
+++ site_sistema/apps/perfil/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 from contas.models import MyUser
 
 
+login_required()
 def perfil_view(request, username):
     perfil = get_object_or_404(MyUser.objects.select_related('perfil'), username=username)
     context = {'obj': perfil}
