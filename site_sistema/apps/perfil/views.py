475d817e1dfaceb3db0bbf174b54c8a8bb134380
adriano-ferrari <adriano-ferrari@outlook.com>
2024-10-13 12:32:54 -0300

    Seção 5: Projeto Parte 2 - Autenticação e Permissões de Acesso | Aula16_criar_perfil_de_usuario

--- site_sistema/apps/perfil/views.py
+++ site_sistema/apps/perfil/views.py
@@ -4,5 +4,5 @@ from contas.models import MyUser
 
 def perfil_view(request, id):
     perfil = get_object_or_404(MyUser.objects.select_related('perfil'), id=id)
-    context = {'obg': perfil}
-    return render(request, 'perfil.html', context)
+    context = {'obj': perfil}
+    return render(request, 'perfil/perfil.html', context)
