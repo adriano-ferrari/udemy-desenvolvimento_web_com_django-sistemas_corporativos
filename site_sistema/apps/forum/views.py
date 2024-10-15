40c29a6e66694ee23fe1514f08a60b8829144b45
Adriano Ferrari <irarrefonairda@gmail.com>
2024-10-15 08:35:45 -0300

    Seção 7: Projeto Parte 4 - Postagens | Aula33_rota_criar_postagem

--- site_sistema/apps/forum/views.py
+++ site_sistema/apps/forum/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from forum import models
 
 # Create your views here.
+def lista_postagem_forum(request):
+    postagens = models.PostagemForum.objects.filter(ativo=True)
+    context = {'postagens': postagens}
+    return render(request, 'forum/lista-postagem-forum.html', context)
+
