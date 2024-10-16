eb230ea6725e435c5937505583ef3517128a0bf3
adriano-ferrari <adriano-ferrari@outlook.com>
2024-10-16 08:37:13 -0300

    Seção 7: Projeto Parte 4 - Postagens | Aula38-rota_editar_postagem

--- site_sistema/apps/forum/views.py
+++ site_sistema/apps/forum/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from base.utils import add_form_errors_to_messages
 
@@ -35,3 +36,31 @@ def criar_postagem_forum(request):
 def detalhe_postagem_forum(request, id):
     postagem = get_object_or_404(models.PostagemForum, id=id)
     return render(request, 'forum/detalhe-postagem-forum.html', {'postagem': postagem})
+
+
+# Editar Postagem (ID)
+@login_required
+def editar_postagem_forum(request, id):
+    postagem = get_object_or_404(models.PostagemForum, id=id)
+    
+    # Verifica se o usuário autenticado é o autor da postagem
+    if request.user != postagem.usuario and not (
+            ['administrador', 'colaborador'] in request.user.groups.all() 
+						or request.user.is_superuser):
+            
+            messages.warning(request, 'O seu usuario nao tem permissao para acessar esta pagina!')
+# Redireciona para uma página de erro ou outra página adequada
+            return redirect('lista-postagem-forum')  
+    
+    if request.method == 'POST':
+        form = PostagemForumForm(request.POST, instance=postagem)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'O seu Post '+ postagem.titulo +' \
+                foi atualizado com sucesso!')
+            return redirect('editar-postagem-forum', id=postagem.id)
+        else:
+            add_form_errors_to_messages(request, form)
+    else:
+        form = PostagemForumForm(instance=postagem)
+    return render(request, 'forum/form-postagem-forum.html', {'form': form})
