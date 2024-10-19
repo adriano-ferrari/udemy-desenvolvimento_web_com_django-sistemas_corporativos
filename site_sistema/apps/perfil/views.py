1354765465384292659170b8b141da7ef0b5015f
adriano-ferrari <adriano-ferrari@outlook.com>
2024-10-19 19:10:50 -0300

    Seção 7: Projeto Parte 4 - Postagens | Aula45_editar_postagem_modal-rota_perfil_do_usuario

--- site_sistema/apps/perfil/views.py
+++ site_sistema/apps/perfil/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
+
 from contas.models import MyUser
+from forum.forms import PostagemForumForm
 
 
 login_required()
 def perfil_view(request, username):
     fitro = MyUser.objects.select_related('perfil').prefetch_related('user_postagem_forum')
     perfil = get_object_or_404(fitro, username=username)
-    context = {'obj': perfil}
-    return render(request, 'perfil/perfil.html', context)
+
+    form_dict = {}
+    for el in perfil.user_postagem_forum.all():
+        form = PostagemForumForm(instance=el) 
+        form_dict[el] = form
+
+    return render(request, 'perfil/perfil.html', {'obj': perfil,'form_dict':form_dict})
