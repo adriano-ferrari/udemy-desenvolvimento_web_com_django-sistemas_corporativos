f973a230911b26f1a42803f8e58e8440ddf2534f
adriano-ferrari <adriano-ferrari@outlook.com>
2024-10-20 13:05:53 -0300

    Seção 9: Projeto Parte 5 - Postagens (Melhorias) Parte 2  | Aula49_multiplos_anexos_na_postagem-criar_modelo

--- site_sistema/apps/forum/admin.py
+++ site_sistema/apps/forum/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from forum.models import PostagemForum
+from .models import PostagemForum, PostagemForumImagem
+
+class PostagemForumImagemInline(admin.TabularInline):
+    model = PostagemForumImagem
+    extra = 0
+
+class PostagemForumAdmin(admin.ModelAdmin):
+    inlines = [
+        PostagemForumImagemInline,
+    ]
+
 
 # Register your models here.
-admin.site.register(PostagemForum)
+admin.site.register(PostagemForum, PostagemForumAdmin)
+#admin.site.register(PostagemForumImagem)
+
+
